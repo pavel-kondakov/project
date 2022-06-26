966fea57495dee2a73ddf766001f9745bd16f774
pavel-kondakov <kondakovtms@gmail.com>
2022-06-26 19:56:41 +0300

    feat: adding Footer file

--- project/src/App.js
+++ project/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+
 import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <Header />
+        <Footer />
     </div>
   );
 }
