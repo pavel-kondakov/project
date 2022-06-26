605a3aba47f0cdbcd238abe2d0db68b2609125ba
pavel-kondakov <kondakovtms@gmail.com>
2022-06-26 19:37:08 +0300

    testing commit

--- project/src/App.js
+++ project/src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import Header from "./components/Header/Header"
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Header />
         <a
           className="App-link"
           href="https://reactjs.org"
